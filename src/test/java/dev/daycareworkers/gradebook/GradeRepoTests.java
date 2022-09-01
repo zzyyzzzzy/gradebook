d9fc591ac4fa162fd928ffe5733e36a1cf4245a6
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-09-01 12:00:08 -0700

    Delete test

--- src/test/java/dev/daycareworkers/gradebook/GradeRepoTests.java
+++ src/test/java/dev/daycareworkers/gradebook/GradeRepoTests.java
@@ -1,5 +1,7 @@
 package dev.daycareworkers.gradebook;
 
+import dev.daycareworkers.entities.Behavior;
+import dev.daycareworkers.entities.Grade;
 import dev.daycareworkers.entities.Student;
 import dev.daycareworkers.repos.GradeRepo;
 import dev.daycareworkers.repos.StudentRepo;
@@ -20,10 +22,13 @@ public class GradeRepoTests {
     GradeRepo gradeRepo;
 
     @Test
-    void delete_student_test(){
+    void delete_grades_by_sid(){
+//        insert into grade values(default, 19, 0, 'grade', 'Bad');
         Student student = new Student(0,"Billy","Bobson","William Bobson");
         Student savedStudent = this.studentRepo.save(student);
+        Grade grade = new Grade(1,savedStudent.getSid(), 0, "test", Behavior.BAD);
+        Grade savedGrade = this.gradeRepo.save(grade);
         this.gradeRepo.deleteBySid(savedStudent.getSid());
-        Assertions.assertFalse(this.studentRepo.existsById(savedStudent.getSid()));
+        Assertions.assertFalse(this.gradeRepo.existsById(savedGrade.getGid()));
     }
 }

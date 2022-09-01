b1ce078eadfdf2a89244de58d04c5f0fa1a6a4f0
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-09-01 07:16:02 -0700

    catch the exception when the foreign key constraint is not satisfied

--- src/main/java/dev/daycareworkers/services/StudentServiceImpl.java
+++ src/main/java/dev/daycareworkers/services/StudentServiceImpl.java
@@ -3,15 +3,19 @@ package dev.daycareworkers.services;
 import dev.daycareworkers.entities.Student;
 import dev.daycareworkers.exceptions.StudentNotFoundException;
 import dev.daycareworkers.repos.StudentRepo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.rmi.StubNotFoundException;
+import java.sql.SQLException;
 
 @Service
 public class StudentServiceImpl implements StudentService {
     @Autowired
     StudentRepo studentRepo;
+    Logger logger = LogManager.getLogger();
 
     @Override
     public Student registerStudent(Student student) {
@@ -22,8 +26,14 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public boolean deleteStudentById(int id) {
         if(this.studentRepo.existsById(id)){
-            this.studentRepo.deleteById(id);
-            return true;
+            try{
+                this.studentRepo.deleteById(id);
+                return true;
+            }catch (RuntimeException err){
+                // catch the exception when the foreign key constraint is not satisfied
+                logger.error("Something went wrong when deleting a student with id " + id);
+                return false;
+            }
         } else{
             throw new StudentNotFoundException();
         }

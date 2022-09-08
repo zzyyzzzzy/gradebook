d9ce31ec4d0c60234456c94f2dceb2db79dcb04c
Jouosh <joshua.martin.2016@gmail.com>
2022-09-08 15:12:02 -0400

    got messages working on all needed routes

--- src/main/java/dev/daycareworkers/controllers/LoginController.java
+++ src/main/java/dev/daycareworkers/controllers/LoginController.java
@@ -4,21 +4,29 @@ import dev.daycareworkers.dtos.LoginCredentials;
 import dev.daycareworkers.dtos.Token;
 import dev.daycareworkers.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @CrossOrigin
 public class LoginController {
 
     @Autowired
+    JmsTemplate jmsTemplate;
+    @Autowired
     LoginService loginService;
 
-    @PostMapping("/login") // use someone's credentials to generate a custom JWT for them
+    @PostMapping("/login")
     public Token login(@RequestBody LoginCredentials loginCredentials) {
-        System.out.println("inside controller" + loginCredentials);
+        Date currentTime = new Date(System.currentTimeMillis());
+        String message = "User " + loginCredentials.getUsername() + "logged in. Time: " + currentTime.toString();
+        jmsTemplate.convertAndSend("important-event-queue", message);
+
         return loginService.authenticateUser(loginCredentials);
     }
 }
--- src/test/java/dev/daycareworkers/gradebook/GradebookApplicationTests.java
+++ src/test/java/dev/daycareworkers/gradebook/GradebookApplicationTests.java
@@ -7,15 +7,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootTest
 class GradebookApplicationTests {
-
-	@Autowired
-	UserAccountRepo userAccountRepo;
-
-//	@Autowired
-//	JwtService jwtService;
 
 	@Test
 	void contextLoads() {
